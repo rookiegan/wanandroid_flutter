7060f75b185f6adea72e8df3963fd2d3d595b1ab
rookiegan <rookiegan@gmail.com>
2022-11-24 16:38:21 +0800

    feat: 添加HomePage

--- lib/modules/_home/home_page.dart
+++ lib/modules/_home/home_page.dart
@@ -1,74 +1,67 @@
 import '../../dependency.dart';
-import '../_test/test_page.dart';
-import '../_test/test_index.dart';
 
 class HomePage extends StatefulWidget {
   static final routeName = "/HomePage";
 
-  HomePage({Key? key}) : super(key: key);
-
   @override
-  _HomePageState createState() => _HomePageState();
+  State<HomePage> createState() {
+    return _HomePageState();
+  }
 }
-
 class _HomePageState extends State<HomePage> {
-  int _counter = 0;
+  static PageController _pageController = PageController();
+  final List<Widget> _bottomNavPages = [
+    Container(color: Colors.lightBlue),
+    Container(color: Colors.red),
+    Container(color: Colors.yellow),
+    Container(color: Colors.green),
+  ];
+
+  int _pageIndex = 0;
+
+  final List<BottomNavigationBarItem> _bottomNavBarItems = [
+    BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+    BottomNavigationBarItem(icon: Icon(Icons.account_circle_rounded), label: "Tab2"),
+    BottomNavigationBarItem(icon: Icon(Icons.account_balance), label: "Tab3"),
+    BottomNavigationBarItem(icon: Icon(Icons.account_balance), label: "Tab4"),
+  ];
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text("Home"),
+      body: PageView.builder(
+        physics: NeverScrollableScrollPhysics(),
+        controller: _pageController,
+        itemCount: _bottomNavPages.length,
+        itemBuilder: (context, index) => _bottomNavPages[index],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        items: _bottomNavBarItems,
+        currentIndex: _pageIndex,
+        selectedItemColor: Colors.blue,
+        onTap: (index) {
+          if (_pageController.hasClients) {
+            _pageIndex = index;
+            _pageController.animateToPage(
+              index,
+              duration: const Duration(milliseconds: 200),
+              curve: Curves.easeInOut,
+            );
+            setState(() {});
+          }
+        },
       ),
       floatingActionButton: _buildDebugFAB(),
     );
   }
 
-  _buildDebugFAB() {
+  Widget _buildDebugFAB() {
     return FloatingActionButton(
       onPressed: () {
-        Get.toNamed(TestPage.routeName);
-        // wanApi.getBannerArticles().asStream().delay(Duration(seconds: 2)).data().loadingDialog().commit((event) {
-        //   "event=== ${event.length}".d();
-        // });
+        wanApi.getBannerArticles().asStream().delay(Duration(seconds: 2)).data().loadingDialog().commit((event) {
+          "event=== ${event.length}".d();
+        });
       },
       child: const Text("Debug"),
       tooltip: "Debug",
